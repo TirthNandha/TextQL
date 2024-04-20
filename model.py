d3f0df9600dc6e7e8543823bcd0262045215432d
nisuJaiswal <nisuJaiswal4@gmail.com>
2024-04-20 22:20:52 +0530

    Running well, but not accurat

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-
 # Load the pre-trained model and tokenizer
 model_name = "gpt2-large"
 model = GPT2LMHeadModel.from_pretrained(model_name)
@@ -11,16 +10,26 @@ tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
+
 def generate_sql_from_text(prompt, max_length=100):
     # Tokenize the input text
-    input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
+    input_ids = tokenizer.encode(
+        "Write SQL query for " + prompt, return_tensors="pt"
+    ).to(device)
 
     # Generate text based on the input
-    output = model.generate(input_ids, max_length=max_length, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+    output = model.generate(
+        input_ids,
+        max_length=max_length,
+        num_return_sequences=1,
+        pad_token_id=tokenizer.eos_token_id,
+    )
 
     # Decode the generated text
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     return generated_text
-prompt = "write sql query to display the employees whose salary is more than 30000"
-generated_sql = generate_sql_from_text(prompt)
-print(generated_sql)
+
+
+# prompt = "write sql query to display students having marks more than 70"
+# generated_sql = generate_sql_from_text(prompt)
+# print(generated_sql)
